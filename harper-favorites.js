fb0918bcc6dc8173d5af8936dbc7c0f3e2d66819
James Harper <jharp949@gmail.com>
2023-03-31 00:07:05 -0700

    new file:   .editorconfig 	new file:   global.css 	new file:   harper-favorites.css 	new file:   harper-favorites.html 	new file:   harper-favorites.js

---
+++ harper-favorites.js
@@ -0,0 +1,22 @@
+//Title: harper.favorites.js
+//Author: James Harper
+//Date: 3/30/2023
+//Description: Web 231 - Week 3 - Assignment 3.2
+
+//Creates a function that uses the alert method to display a message. Each function is linked to the
+//respective button through an onclick event on harper-favorites.html.
+function team() {
+  alert('Dodgers');
+}
+
+function book() {
+  alert('East of Eden');
+}
+
+function author() {
+  alert('John Steinbeck');
+}
+
+function progLang() {
+  alert('Python');
+}
